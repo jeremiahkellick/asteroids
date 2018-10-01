a84fb7796ae8901377f1da105cb6c12176fb6f76
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-09-30 17:16:28 -0700

    Configure npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: './lib/asteroids.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: "./lib/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
